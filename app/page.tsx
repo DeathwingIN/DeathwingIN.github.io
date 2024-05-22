e7b32ddc8571d28e143392723a0f24b8b741d972
DeathwingIN <imeshnirmal1u@gmail.com>
2024-05-23 00:28:45 +0530

    Coming soon

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import GlobeDemo from "@/components/GitHubGlobe";
+import {TypewriterEffect} from "@/components/ui/typewriter-effect";
+import TypewriterEffectSmoothDemo from "@/components/TypeWritter";
+
+export default function Home() {
+    return (
+        <>
+            <TypewriterEffectSmoothDemo/>
+            <GlobeDemo/>
+        </>
+
+    );
+}
