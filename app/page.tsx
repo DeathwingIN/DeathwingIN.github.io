b9cb5dd2973ff70b8927a6ce22957c10d1ed16c0
DeathwingIN <imeshnirmal1u@gmail.com>
2024-06-10 00:04:52 +0530

    Add Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import GlobeDemo from "@/components/GitHubGlobe";
 import {TypewriterEffect} from "@/components/ui/typewriter-effect";
 import TypewriterEffectSmoothDemo from "@/components/TypeWritter";
+import Hero from "@/components/Hero";
+
 
 export default function Home() {
     return (
-        <>
-            <TypewriterEffectSmoothDemo/>
-            <GlobeDemo/>
-        </>
+        <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <div className="max-w-7xl w-full">
+                <Hero />
+            </div>
+        </main>
 
     );
 }
