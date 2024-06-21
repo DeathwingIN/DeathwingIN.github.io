6b9f5437b32ad46e67d17467617d09747bc2cf4a
DeathwingIN <imeshnirmal1u@gmail.com>
2024-06-21 18:19:06 +0530

    Done 00 UP

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -8,8 +8,10 @@ import {
     IconBrandGoogle,
     IconBrandOnlyfans,
 } from "@tabler/icons-react";
+import MagicButton from "./MagicButton";
 
 import {GlobeDemo} from "@/components/Globe";
+import { FaLocationArrow } from "react-icons/fa6";
 
 
 const ContactMe = () => {
@@ -19,55 +21,71 @@ const ContactMe = () => {
     };
 
     return (
-        <section className="flex flex-col sm:flex-row justify-center items-center gap-2  relative">
-            <div className="flex-3 md:flex-1">
-                <motion.h1
-                    initial={{opacity: 0.5, y: 100}}
-                    whileInView={{opacity: 1, y: 0}}
-                    transition={{
-                        delay: 0.3,
-                        duration: 0.8,
-                        ease: "easeInOut",
-                    }}
-                    className="relative max-w-md w-full h-full mx-auto rounded-none md:rounded-2xl shadow-input bg-white dark:bg-black xl:mt-[200px] z-20"
-                >
-                    <form onSubmit={handleSubmit} className="relative z-30 p-4 md:p-8 xl:mt-[200px]">
-                        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                            <LabelInputContainer>
-                                <Label htmlFor="firstname">First name</Label>
-                                <Input id="firstname" placeholder="Tyler" type="text"/>
+        <section className={ "mt-10"}>
+            <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl text-center ">
+                Skills & Tools
+            </h1>
+            <p className="leading-7 text-center">
+                My primary focus lies in frontend development and backend development and Learning Every Day
+            </p>
+            <div className="flex flex-col sm:flex-row justify-center items-center gap-2  relative">
+                <div className="flex-3 md:flex-1">
+                    <motion.h1
+                        initial={{opacity: 0.5, y: 100}}
+                        whileInView={{opacity: 1, y: 0}}
+                        transition={{
+                            delay: 0.3,
+                            duration: 0.8,
+                            ease: "easeInOut",
+                        }}
+                        className="relative max-w-md w-full h-full mx-auto rounded-none md:rounded-2xl shadow-input bg-white dark:bg-black xl:mt-[200px] z-20"
+                    >
+                        <p className="font-bold text-xl text-neutral-800 dark:text-neutral-200 text-center">
+                            Welcome to Aceternity
+                        </p>
+                        <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300   pl-4 ">
+                            Login to aceternity if you can because we don&apos;t have a login flow
+                            yet
+                        </p>
+                        <form onSubmit={handleSubmit} className="relative z-30 p-4 md:p-8 ">
+                            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+                                <LabelInputContainer>
+                                    <Label htmlFor="firstname">First name</Label>
+                                    <Input id="firstname" placeholder="Tyler" type="text"/>
+                                </LabelInputContainer>
+                                <LabelInputContainer>
+                                    <Label htmlFor="lastname">Last name</Label>
+                                    <Input id="lastname" placeholder="Durden" type="text"/>
+                                </LabelInputContainer>
+                            </div>
+                            <LabelInputContainer className="mb-4">
+                                <Label htmlFor="email">Email Address</Label>
+                                <Input id="email" placeholder="projectmayhem@fc.com" type="email"/>
                             </LabelInputContainer>
-                            <LabelInputContainer>
-                                <Label htmlFor="lastname">Last name</Label>
-                                <Input id="lastname" placeholder="Durden" type="text"/>
+                            <LabelInputContainer className="mb-4">
+                                <Label htmlFor="confirmEmail">Confirm Email Address</Label>
+                                <Input id="confirmEmail" placeholder="projectmayhem@fc.com" type="email"/>
                             </LabelInputContainer>
-                        </div>
-                        <LabelInputContainer className="mb-4">
-                            <Label htmlFor="email">Email Address</Label>
-                            <Input id="email" placeholder="projectmayhem@fc.com" type="email"/>
-                        </LabelInputContainer>
-                        <LabelInputContainer className="mb-4">
-                            <Label htmlFor="confirmEmail">Confirm Email Address</Label>
-                            <Input id="confirmEmail" placeholder="projectmayhem@fc.com" type="email"/>
-                        </LabelInputContainer>
 
-                        <button
-                            className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
-                            type="submit"
-                        >
-                            Send Message &rarr;
-                            <BottomGradient/>
-                        </button>
 
-                        <div
-                            className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full"/>
-                    </form>
-                </motion.h1>
+                            <MagicButton
+                                title="Send Message"
+                                icon={<FaLocationArrow />}
+                                position="right"
+                            />
 
+                            <div
+                                className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full"/>
+                        </form>
+                    </motion.h1>
+
+                </div>
+                <div className="hidden sm:flex flex-1 w-1/4">
+                    <GlobeDemo/>
+                </div>
             </div>
-            <div className="hidden sm:flex flex-1 w-1/4">
-                <GlobeDemo/>
-            </div>
+
+
         </section>
 
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,55 +1,69 @@
-import { FaLocationArrow } from "react-icons/fa6";
+import {FaLocationArrow} from "react-icons/fa6";
 
-import { socialMedia } from "@/data";
+import {socialMedia} from "@/data";
 import MagicButton from "./MagicButton";
 
 const Footer = () => {
-  return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
-      <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
-        </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
-        </p>
-        <a href="mailto:contact@jsmastery.pro">
-          <MagicButton
-            title="Let's get in touch"
-            icon={<FaLocationArrow />}
-            position="right"
-          />
-        </a>
-      </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Adrian Hajdin
-        </p>
-
-        <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
+    return (
+        <footer className="w-full pt-20 pb-10" id="contact">
+            {/* background grid */}
+            <div className="w-full absolute left-0 -bottom-72 min-h-96">
+                <img
+                    src="/footer-grid.svg"
+                    alt="grid"
+                    className="w-full h-full opacity-50 "
+                />
             </div>
-          ))}
-        </div>
-      </div>
-    </footer>
-  );
+
+            <div className="flex flex-col items-center">
+                <h1 className="heading lg:max-w-[45vw]">
+                    Ready to take <span className="text-purple">your</span> digital
+                    presence to the next level?
+                </h1>
+                <p className="text-white-200 md:mt-10 my-5 text-center">
+                    Reach out to me today and let&apos;s discuss how I can help you
+                    achieve your goals.
+                </p>
+                <a href="https://chatgpt.com" target={'blank'}>
+                    <button
+                        className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                <span className="absolute inset-0 overflow-hidden rounded-full">
+                <span
+                    className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"/>
+                </span>
+                        <div
+                            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                    <span>
+                   Download CV
+                    </span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                fill="#fff"
+                                stroke="#fff"
+                                version="1.1"
+                                viewBox="0 0 43.916 43.916"
+                                xmlSpace="preserve"
+                            >
+                                <path
+                                    d="M34.395 0H9.522a5 5 0 00-5 5v33.916a5 5 0 005 5h24.871a5 5 0 005-5V5a4.999 4.999 0 00-4.998-5zM9.208 16.855c0-1.172.951-2.121 2.121-2.121h.742a4.906 4.906 0 01-1.277-3.304 4.932 4.932 0 019.864.001 4.899 4.899 0 01-1.279 3.303h.709a2.12 2.12 0 012.121 2.121v3.578c0 1.122-.875 2.03-1.975 2.106h-9.051a2.115 2.115 0 01-1.975-2.106v-3.578h0zm23.5 20.561h-21.5a2 2 0 010-4h21.5a2 2 0 010 4zm0-7.5h-21.5a2 2 0 010-4h21.5a2 2 0 010 4zm0-7.5h-6.5a2 2 0 010-4h6.5a2 2 0 010 4z"></path>
+                            </svg>
+                        </div>
+                        <span
+                            className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"/>
+                    </button>
+
+                </a>
+
+            </div>
+            <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+                <p className="md:text-base text-sm md:font-normal font-light">
+                    Copyright © 2024 Imesh Nirmal
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
